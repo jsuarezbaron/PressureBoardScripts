1810ec1e6d3de72ce0bb8632c78944c9d5a84412
jsuarezbaron <jsuarezbaron@gmail.com>
2019-05-22 22:06:18 -0300

    Se agrega fclose(placa) al final del script para evitar que Matlab mantenga ocupado el puerto serie.

--- SerialPr.m
+++ SerialPr.m
@@ -1,10 +1,8 @@
-%%%
-%
-placa = serial('COM5','BaudRate',9600);
+clear all
+instrreset;
+placa = serial('COM8','BaudRate',9600);  %Colocar el COM que aparece en Device Manager
 fopen(placa);
 
-%%
-%
 
 figure(1)
 hold on
@@ -12,12 +10,13 @@ tic
 for i = 1:100
     
     str = query(placa, 'P');
-    if (str == '')
-    else
+    if (str(1) > 48 && str(1) < 57)
         presiones = str2num(str)
         tiempos = toc ;
-        plot(tiempos, presiones(:,1),'.');
+        plot(tiempos, presiones(:,1),'.');      
     end
     pause(1)
     
 end
+
+fclose(placa);
