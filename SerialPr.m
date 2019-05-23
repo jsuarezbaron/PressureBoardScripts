1d27bd55b0c1e819f8378b5859273f5f1f401928
jsuarezbaron <jsuarezbaron@gmail.com>
2019-05-23 00:16:24 -0300

    Misma versión anterior.

--- SerialPr.m
+++ SerialPr.m
@@ -3,20 +3,22 @@ instrreset;
 placa = serial('COM8','BaudRate',9600);  %Colocar el COM que aparece en Device Manager
 fopen(placa);
 
-
+%%%
 figure(1)
 hold on
 tic
 for i = 1:100
-    
     str = query(placa, 'P');
+    strP = sprintf('x%d',str );
     if (str(1) > 48 && str(1) < 57)
         presiones = str2num(str)
         tiempos = toc ;
         plot(tiempos, presiones(:,1),'.');      
     end
+    print(strP)
     pause(1)
     
 end
 
+%%%
 fclose(placa);
